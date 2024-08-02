0e46ef62168802cad5a9f7465cacd1650d3d95f0
TarzanIllges <116415903+TarzanIllges@users.noreply.github.com>
2024-08-02 14:06:35 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping/", // Update with your GitHub Pages URL path
+  base:" https://github.com/TarzanIllges/e-plantShopping.git" // Update with your GitHub Pages URL path
   build: {
     outDir: 'dist' // Ensure this matches the deploy script
   },
