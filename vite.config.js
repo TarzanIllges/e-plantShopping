fbb1d8eeb1721ea1987a92c090ea44283ffecb84
TarzanIllges <116415903+TarzanIllges@users.noreply.github.com>
2024-08-02 13:18:20 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/e-plantShopping/",
+  base: "TarzanIllges/e-plantShopping",
   plugins: [react()],
   build: {
     outDir: 'build'
